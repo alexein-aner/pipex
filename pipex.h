64b33ec8509e0d095a10d1f968cb244bfcd89dd8
Kamil <nievekamil@gmail.com>
2024-10-16 13:29:46 +0200

    safety net

--- pipex.h
+++ pipex.h
@@ -6,23 +6,23 @@
 /*   By: knieve-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 15:34:03 by knieve-l          #+#    #+#             */
-/*   Updated: 2024/10/15 16:29:48 by knieve-l         ###   ########.fr       */
+/*   Updated: 2024/10/16 12:48:29 by kamil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <unistd.h>  // For fork, execvp, pipe, dup2, close
-# include <sys/types.h>  // For pid_t
-# include <sys/wait.h>   // For wait
-# include <fcntl.h>  // For open, O_RDONLY, O_WRONLY, O_CREAT, O_TRUNC
-# include <stdlib.h>  // For malloc, free, exit
-# include <stdio.h>   // For perror
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
 
-unsigned int		ft_strlcpy(char *dest, char *src, unsigned int size);
-int					ft_strlen(char *str);
-int					ft_strncmp( char *s1,  char *s2, size_t n);
+unsigned int			ft_strlcpy(char *dest, char *src, unsigned int size);
+int				ft_strlen(char *str);
+int				ft_strncmp( char *s1,  char *s2, size_t n);
 void				*ft_calloc(size_t count, size_t size);
 char				*ft_strdup( char *s1);
 char				*ft_substr(char  *s, unsigned int st, size_t len);
